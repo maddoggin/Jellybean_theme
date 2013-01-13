5c5737af862c88b0ebaac28b5f06a62427d26c9d
maddoggin <valozuna361@gmail.com>
2013-01-13 12:54:37 -0600

    initial push

---
+++ Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+	com.tmobile.themes
+
+LOCAL_PACKAGE_NAME := JellyBean
+
+LOCAL_CERTIFICATE := platform
+
+include $(BUILD_PACKAGE)
+
+# including the test apk
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
